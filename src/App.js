128778cb112603df3a9d40297de6c29a58b513ae
MayankMukul <58073442+MayankMukul@users.noreply.github.com>
2024-04-14 23:28:18 +0530

    app update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 
 import './App.css';
+import { useState, useEffect } from 'react';
+// import axios from 'axios'
 import ItemCard from './ItemCard';
 import Navbar from './Navbar';
 import Footer from './Footer';
 
 function App() {
-  let data = {
+
+  const [product, setProduct] = useState([{
     id: 1,
     title: "iPhone 9",
     description: "An apple mobile which is nothing like apple",
@@ -23,15 +26,70 @@ function App() {
       "https://cdn.dummyjson.com/product-images/1/4.jpg",
       "https://cdn.dummyjson.com/product-images/1/thumbnail.jpg",
     ],
-  };
+  }])
+
+  let data = [
+    {
+      id: 1,
+      title: "iPhone 9",
+      description: "An apple mobile which is nothing like apple",
+      price: 549,
+      discountPercentage: 12.96,
+      rating: 4.69,
+      stock: 94,
+      brand: "Apple",
+      category: "smartphones",
+      thumbnail: "https://cdn.dummyjson.com/product-images/1/thumbnail.jpg",
+      images: [
+        "https://cdn.dummyjson.com/product-images/1/1.jpg",
+        "https://cdn.dummyjson.com/product-images/1/2.jpg",
+        "https://cdn.dummyjson.com/product-images/1/3.jpg",
+        "https://cdn.dummyjson.com/product-images/1/4.jpg",
+        "https://cdn.dummyjson.com/product-images/1/thumbnail.jpg",
+      ],
+    },
+    {
+      id: 2,
+      title: "iPhone X",
+      description:
+        "SIM-Free, Model A19211 6.5-inch Super Retina HD display with OLED technology A12 Bionic chip with ...",
+      price: 899,
+      discountPercentage: 17.94,
+      rating: 4.44,
+      stock: 34,
+      brand: "Apple",
+      category: "smartphones",
+      thumbnail: "https://cdn.dummyjson.com/product-images/2/thumbnail.jpg",
+      images: [
+        "https://cdn.dummyjson.com/product-images/2/1.jpg",
+        "https://cdn.dummyjson.com/product-images/2/2.jpg",
+        "https://cdn.dummyjson.com/product-images/2/3.jpg",
+        "https://cdn.dummyjson.com/product-images/2/thumbnail.jpg",
+      ],
+    }
+  ];
+
+  const getData = ()=>{
+    setProduct(data);
+  }
+ 
+  useEffect(() => {
+    getData();
+  }, []);
   return (
     <>
-    <Navbar/>
-      <div className="container m-auto bg-stone-50">
-        <div className="flex flex-wrap justify-center m-auto ">Items</div>
-          <ItemCard data = {data}></ItemCard>
+      <div className="container m-auto  bg-stone-50">
+        <h1 className='bg-black text-white p-2 m-2 rounded font-bold'>
+      Items
+        </h1>
+        <div className="flex flex-wrap justify-center m-auto ">
+        {product.map((item)=>{
+          return <ItemCard data = {item} key = {item.id}></ItemCard>
+          
+        })}
+        </div>
       </div>
-<Footer/>
+
     </>
   );
 }
