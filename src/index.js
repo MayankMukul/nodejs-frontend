0caf689db8a6805487c6b75728db604373f12602
MayankMukul <58073442+MayankMukul@users.noreply.github.com>
2024-04-20 23:23:19 +0530

    routes added

--- src/index.js
+++ src/index.js
@@ -3,11 +3,43 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Outlet, RouterProvider, createBrowserRouter } from 'react-router-dom';
+import Navbar from './Navbar';
+import Footer from './Footer';
+import AddProduct from './AddProduct';
+import ErrorElement from './ErrorElement';
+
+const Applayout = ()=>{
+  return (
+    <div className='flex flex-col h-screen'>
+      <Navbar></Navbar>
+      <Outlet/>
+      <Footer></Footer>
+    </div>
+  )
+}
+
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <Applayout />,
+    errorElement:<ErrorElement/>,
+    children:[
+      {
+        path: "/",
+        element: <App />
+      },{
+        path : "/addproduct",
+        element: <AddProduct />
+      }
+    ]
+  }
+])
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={appRouter}></RouterProvider>
   </React.StrictMode>
 );
 
