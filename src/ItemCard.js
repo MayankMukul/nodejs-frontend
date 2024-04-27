89d78fb57a8d1c1e7d34770da38c4de79968efbf
MayankMukul <58073442+MayankMukul@users.noreply.github.com>
2024-04-27 23:02:06 +0530

    card page update

--- src/ItemCard.js
+++ src/ItemCard.js
@@ -1,15 +1,15 @@
 import React from 'react'
 
 export default function ItemCard(props) {
-    console.log(props.data);
+    // console.log(props);
     return (
-      <div className="bg-stone-300 m-2 p-2 rounded w-52">
+      <div className="bg-stone-300 m-2 p-2 rounded w-60">
         <img
           src={`${props.data.thumbnail}`}
           alt="unable to fetch"
           height="300"
           width="200"
-          className="rounded"
+          className="rounded mx-auto"
         />
         <h1>Name : {props.data.title}</h1>
         <p>Company : {props.data.brand}</p>
@@ -17,8 +17,8 @@ export default function ItemCard(props) {
         <p>Price : {props.data.price}</p>
         <p>Discount : {props.data.discountPercentage}</p>
 
-        <button className="bg-black text-white m-2 p-1 rounded w-2/3 ">
-          Add
+        <button className="bg-black text-white m-2 p-1 rounded w-2/3 " onClick={()=>props.handleClick(props.data._id)}>
+          Delete
         </button>
       </div>
     );
