d602bc99a317bab4eea7a6eec657aa9b594ae1d9
MayankMukul <58073442+MayankMukul@users.noreply.github.com>
2024-04-28 23:35:30 +0530

    error page added

---
+++ src/ErrorElement.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function ErrorElement() {
+  return (
+    <div className='bg-stone-100 h-screen text-center font-semibold text-xl p-4'>
+        404 - Page Not Found
+    </div>
+  )
+}
