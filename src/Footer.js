42a00f7aec422d3c5da7815af8b8ca6f3fb455b5
MayankMukul <58073442+MayankMukul@users.noreply.github.com>
2024-04-29 23:32:18 +0530

    footer added

---
+++ src/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <div>
+      <div className=" flex justify-between p-4 bg-stone-200">
+        <div className=" m-auto">
+          Made by <a href='https://github.com/MayankMukul' target='blank'>@Mayank</a>
+        </div>
+      </div>
+    </div>
+  );
+}
